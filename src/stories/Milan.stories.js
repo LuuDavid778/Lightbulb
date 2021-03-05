efb63a486ccacc4c85f401be7c2a163cabcadb83
milanjot <milanjot@hotmail.ca>
2021-03-04 18:09:03 -0800

    Login Comp done

--- src/stories/Milan.stories.js
+++ src/stories/Milan.stories.js
@@ -5,6 +5,7 @@ import LikeTag from '../comps/LikeTag';
 import PostCaption from '../comps/PostCaption';
 import CommentPrompt from '../comps/CommentPrompt';
 import AvatarSelect from '../comps/AvatarSelect';
+import Login from '../comps/Login';
 
 
 
@@ -17,4 +18,5 @@ export const SimpleLikeTag = () => <LikeTag />
 export const SimplePostCaption = () => <PostCaption/>
 export const SimpleCommentPrompt = () => <CommentPrompt/>
 export const SimpleAvatarSelect = () => <AvatarSelect/>
+export const SimpleLoginForm = () => <Login/>
 
