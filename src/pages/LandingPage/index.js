0eea8030bf408c6f072e67455702984093fe205b
milanjot <milanjot@hotmail.ca>
2021-03-13 17:50:11 -0800

    landing pg

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Logo from './logo.svg';
  
+
 const AppTitle = styled.div`
 font-family: Poppins;
 font-style: normal;
@@ -8,11 +10,15 @@ font-weight: bold;
 font-size: 48px;
 line-height: 72px;
 color: #B086F7;
+margin-left: 59px ;
+margin-top: 42px;
+align-self:center;
 `;
 
 const LandingPage = () => {
  return <div>
- <AppTitle>LIGHTBULB</AppTitle>
+     <AppTitle>LIGHTBULB</AppTitle>
+    <Logo></Logo>
  </div>
 }
 
