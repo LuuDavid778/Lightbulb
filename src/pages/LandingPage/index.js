3831f0f62dcb927719f9c0c1a6c4debadc35911f
milanjot <milanjot@hotmail.ca>
2021-03-13 17:30:32 -0800

    PUSH

---
+++ src/pages/LandingPage/index.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+ 
+const AppTitle = styled.div`
+font-family: Poppins;
+font-style: normal;
+font-weight: bold;
+font-size: 48px;
+line-height: 72px;
+color: #B086F7;
+`;
+
+const LandingPage = () => {
+ return <div>
+ <AppTitle>LIGHTBULB</AppTitle>
+ </div>
+}
+
+LandingPage.defaultProps = {
+
+}
+
+export default LandingPage;
